abdae333b1f0d6727939c346390ea7fd6182ba9e
Andres Vasquez <andres.vasquez@hydra.cloud>
2020-06-30 05:43:09 -0500

    register terminate

--- src/views/signUp/SignUp.js
+++ src/views/signUp/SignUp.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { withStyles } from "@material-ui/core/styles";
 
 import Swal from 'sweetalert2';
+import axios from 'axios';
 
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -28,7 +29,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import ParticlesBg from 'particles-bg';
 
 import { Redirect } from 'react-router-dom';
-import Cookies from 'js-cookie';
+
 
 
 class SignUp extends Component {
@@ -36,7 +37,6 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            authenticated: false,
             toSignup: false,
             email: "",
             password: "",
@@ -50,6 +50,7 @@ class SignUp extends Component {
         this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
         this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this);
         this.registerUser = this.registerUser.bind(this);
+        this.handleToSignup = this.handleToSignup.bind(this);
 
     }
 
@@ -65,46 +66,51 @@ class SignUp extends Component {
         event.preventDefault();
     }
 
+    handleToSignup = () => {
+        this.setState({toSignup : true});
+    }
+
+
     async registerUser() {
-
-        var userJson = 
-        {
-            name : this.state.firstName +" "+ this.state.lastName,
-            rol :  this.state.rol,
-            email : this.state.email,
-            password : this.state.password
-        };
-
+        console.log(this.state.rol.length);
         if (this.state.email === "" &&
             this.state.password === "" &&
             this.state.firstName === "" &&
             this.state.lastName === "" &&
-            this.state.address === "") {
+            this.state.rol.length === 0) {
             Swal.fire("Empty field!", "Please, fill all the fields", "error");
         } else {
             if (this.state.email.includes('@')) {
                 if (this.state.password.length >= 6) {
-                    var url = 'http://localhost:8080/users/save/';
-                    fetch(url, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(userJson)
-                    })
-                        .then(response => response.json())
-                        .then(async data => {
-                            console.log(data);
-                            if (data) {
-                                Swal.fire(
-                                    'Cuenta creada satisfactoriamente!',
-                                    'Sera redireccionado a la pagina de inicio de sesion',
-                                    'success'
-                                )
-                            } else {
-                                Swal.fire("Signup failed!", "try again later", "error");
-                            }
-                        })
+                    let res = await axios.post('https://backend-easyparking.herokuapp.com/users/save/', {
+                        name : this.state.firstName +" "+ this.state.lastName,
+                        rol :  this.state.rol,
+                        email : this.state.email,
+                        password : this.state.password
+                      })
+                      .then(function (response) {
+                        console.log(response.status);
+                        console.log(response.data);
+                          if(response.status===200){
+                            Swal.fire(
+                                'Cuenta creada satisfactoriamente!',
+                                'Sera redireccionado a la pagina de inicio de sesion',
+                                'success'
+                            ).then(function (result)  {
+                                if (result.value) {
+                                    
+                                }
+                              });
+
+                            
+                          }else{
+                            Swal.fire("Signup failed!", "try again later", "error");
+                          }
+                        
+                      })
+                      .catch(function (error) {
+                        console.log(error);
+                      });
                 } else {
                     Swal.fire("Short password!", "The password should be at least 6 digits", "error");
                 }
@@ -112,12 +118,16 @@ class SignUp extends Component {
                 Swal.fire("Not an email!", "Please, type a correct email", "error");
             }
         }
+
+        this.handleToSignup();
+
     }
 
     render() {
         const { classes } = this.props;
         return (
             <div>
+                {this.state.toSignup && <Redirect to='/login'></Redirect>}
                 <ParticlesBg color="#7C00C8" type="circle" bg={true} />
                 <Container component="main" maxWidth="xs">
                     <CssBaseline />
